8bd90b437182fc6817084b798558dbe08a83548e
Rodolfo Unanue <rodolfo.unanue@usach.cl>
2022-10-09 17:53:08 -0300

    [Rodolfo Unanue] - Proyecto de SpringBoot inicializado.

---
+++ control1/src/test/java/cl/tbd/control1/Control1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tbd.control1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Control1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
